d1b42044c99413d57f69320e490ab69c9bcfb47c
Niki Powell <nhe.powell@gmail.com>
2024-06-13 14:25:43 -0500

    added delete button for current user on My Rocks

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -39,18 +39,52 @@ class RockView(ViewSet):
 
         # return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+
     def list(self, request):
         """Handle GET requests for all items
 
         Returns:
             Response -- JSON serialized array
         """
+
+        # Get query string parameter
+        owner_only = self.request.query_params.get("owner", None)
+
         try:
+            # Start with all rows
             rocks = Rock.objects.all()
+
+            # If `?owner=current` is in the URL
+            if owner_only is not None and owner_only == "current":
+                # Filter to only the current user's rocks
+                rocks = rocks.filter(user=request.auth.user)
+
             serializer = RockSerializer(rocks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+            
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            rock = Rock.objects.get(pk=pk)
+            if rock.user.id == request.auth.user.id:
+                rock.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'message': 'You do not own that rock'}, status=status.HTTP_403_FORBIDDEN)
+
+        except Rock.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # import Type model at the top of the view and serialize it
 class RockTypeSerializer(serializers.ModelSerializer):
@@ -75,7 +109,8 @@ class RockSerializer(serializers.ModelSerializer):
 
     user = RockOwnerSerializer(many=False)
     # because type has a relationship field (foreign key to rock model), need to specify how to serialize this relationship:
-    type = serializers.ReadOnlyField(source='type.label') #example for a FK field
+    # type = serializers.ReadOnlyField(source='type.label') #example for a FK field
+    type = RockTypeSerializer(many=False)
 
     class Meta:
         model = Rock
